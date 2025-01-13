9fafc2d446de0b912525dcc43abd5efdd5a5e164
David Martinez <davmartinezb99@gmail.com>
2025-01-13 18:20:38 -0300

    Adicionado lista de favoritos, e relações entre os modelos

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_01_13_205214) do
+ActiveRecord::Schema[8.0].define(version: 2025_01_13_211416) do
+  create_table "events", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "event_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["event_id"], name: "index_favorites_on_event_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
+
   create_table "profiles", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "city"
@@ -35,5 +50,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_13_205214) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "favorites", "events"
+  add_foreign_key "favorites", "users"
   add_foreign_key "profiles", "users"
 end
