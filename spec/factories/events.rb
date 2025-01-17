5bf2cddc1f045204792e6f6dec7ce67e0825dd30
David Alejandro Martinez Bolivar <davmartinezb99@gmail.com>
2025-01-17 10:07:51 -0300

    Adicionada sequência para event_id e refatoração do initialize_with.
    Co-authored-by: César Faustino <cesar.faustino@fatec.sp.gov.br>

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -1,16 +1,10 @@
 FactoryBot.define do
-  factory :event do
-    event_id { 1 }
+  factory :event, class: Event do
+    sequence(:event_id) { |n|  n }
     name { "Dev Week" }
     banner { 'http://localhost:3000/events/1/banner.jpg' }
     logo { 'http://localhost:3000/events/1/logo.jpg' }
-    initialize_with do
-      new(
-        event_id: event_id,
-        name: name,
-        banner: banner,
-        logo: logo
-      )
-    end
+    initialize_with { new(event_id: event_id, name:
+                      name, banner: banner, logo: logo) }
   end
 end
