ac12b539f729027ada05bdf7755bea05e9c1615c
David Martinez <davmartinezb99@gmail.com>
2025-01-17 17:28:30 -0300

    Adicionado estilo no texto da data do evento, na tela de listagem
    Co-authored-by: César Faustino <cesar.faustino@fatec.sp.gov.br>

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,6 @@
 class EventsController < ApplicationController
   def index
-    @events = Event.all
+    @events = mock_events
   end
 
   def show
@@ -11,11 +11,11 @@ class EventsController < ApplicationController
 
   def mock_events
     [
-    Event.new(event_id: 1, name: "Dev Week", banner: "http://localhost:3000/events/1/banner.jpg",
-    logo: "http://localhost:3000/events/1/logo.jpg", start_date: 2.days.from_now,
+    Event.new(event_id: 1, name: "Dev Week", banner: "https://img.freepik.com/vector-gratis/banner-negocios-generales_23-2148994466.jpg",
+    logo: "https://i.pinimg.com/736x/d3/49/36/d349363dc3c7297aa97363e6ec641d9d.jpg", start_date: 2.days.from_now,
     end_date: 5.days.from_now),
-    Event.new(event_id: 2, name: "Ruby Update", banner: "http://localhost:3000/events/2/banner.jpg",
-    logo: "http://localhost:3000/events/2/logo.jpg", start_date: 5.days.from_now,
+    Event.new(event_id: 2, name: "Ruby Update", banner: "https://img.freepik.com/psd-gratuitas/modelo-de-banner-horizontal-para-conscientizacao-do-dia-da-aids_23-2149141293.jpg",
+    logo: "https://i.pinimg.com/736x/d3/49/36/d349363dc3c7297aa97363e6ec641d9d.jpg", start_date: 5.days.from_now,
     end_date: 7.days.from_now)
     ]
   end
