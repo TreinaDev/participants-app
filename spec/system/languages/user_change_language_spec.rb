a32a8ed673e902f107599001666eb3a389b54cac
SamuelRocha91 <samuel_sr@hotmail.com.br>
2025-01-20 18:23:51 -0300

    feat: inclui teste pra verificar troca de idioma
    Co-authored-by: Gabriel-T-P <gabrielthiago2000@hotmail.com>

--- spec/system/languages/user_change_language_spec.rb
+++ spec/system/languages/user_change_language_spec.rb
@@ -2,12 +2,16 @@ require 'rails_helper'
 
 describe 'Usuário troca de idioma' do
   it 'com sucesso' do
-    visit root_path
+    visit events_path(locale: 'pt-BR')
     within('nav') do
       click_on 'English'
     end
 
     expect(page).to have_css 'h1', text: 'Available Events'
     expect(page).not_to have_css 'h1', text: 'Eventos disponíveis'
+    within('nav') do
+      expect(page).to have_link 'Português'
+      expect(page).not_to have_link 'English'
+    end
   end
 end
