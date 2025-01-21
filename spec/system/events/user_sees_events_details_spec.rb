69ed0d542804e010ee141b25e1d577a0b29838d3
SamuelRocha91 <samuel_sr@hotmail.com.br>
2025-01-21 10:28:23 -0300

    refactor: traduz views pelo I18n
    Co-authored-by: Gabriel-T-P <gabrielthiago2000@hotmail.com>

--- spec/system/events/user_sees_events_details_spec.rb
+++ spec/system/events/user_sees_events_details_spec.rb
@@ -22,7 +22,7 @@ describe 'Usuário acessa página de detalhes de um evento' do
     expect(page).to have_content 'Aprendedo a cozinhar'
     expect(page).to have_content 'Local: Rua dos morcegos, 137, CEP: 40000000, Salvador, Bahia, Brasil'
     expect(page).to have_content 'Aprenda a fritar um ovo'
-    expect(page).to have_content 'Criado por: Samuel'
+    expect(page).to have_content 'Dono do evento: Samuel'
     expect(page).to have_content 'Limite de Participantes: 30'
     expect(page).to have_css 'img[src="http://localhost:3000/events/1/banner.jpg"]'
     expect(page).to have_content 'http://evento_fake.com'
