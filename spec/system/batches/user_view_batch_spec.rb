e0595feba0b6922cbf748e5f96704896f6ac7084
SamuelRocha91 <samuel_sr@hotmail.com.br>
2025-01-22 17:54:57 -0300

    feat: cria casos de muitos ingressos e fora da época de venda
    Co-authored-by: Gabriel-T-P <gabrielthiago2000@hotmail.com>

--- spec/system/batches/user_view_batch_spec.rb
+++ spec/system/batches/user_view_batch_spec.rb
@@ -35,7 +35,7 @@ describe 'usuário vê botão de compra de ingressos' do
     expect(page).not_to have_content "Ingressos disponíveis #{I18n.l(1.day.from_now.to_date, format: :short)}"
   end
 
-  it 'com ingressos fora da época de venda' do
+  it 'com um único ingresso fora da época de venda' do
     batch = [ {
       batch_id: 1,
       name: 'Lote Teste',
@@ -60,5 +60,54 @@ describe 'usuário vê botão de compra de ingressos' do
     expect(page).not_to have_link 'Ver Ingressos'
     expect(page).not_to have_link 'Ingressos Esgotados'
     expect(page).to have_content "Ingressos disponíveis #{I18n.l(5.day.from_now.to_date, format: :short)}"
+  end
+
+  it 'com vários ingressos fora da época de venda' do
+    batch = [ {
+      batch_id: 1,
+      name: 'Lote Teste',
+      limit_tickets: 30,
+      start_date: 5.day.from_now.to_date,
+      value: 10.00,
+      end_date: 1.month.from_now.to_date,
+      event_id: 1
+    }, {
+      batch_id: 2,
+      name: 'Mesmo Lote Teste',
+      limit_tickets: 50,
+      start_date: 3.day.from_now.to_date,
+      value: 20.00,
+      end_date: 3.month.from_now.to_date,
+      event_id: 1
+    }, {
+      batch_id: 3,
+      name: 'Mesmo Mesmo Lote Teste',
+      limit_tickets: 50,
+      start_date: 2.day.from_now.to_date,
+      value: 20.00,
+      end_date: 2.month.from_now.to_date,
+      event_id: 1
+    } ]
+    event = build(:event, name: 'Evento Teste 01', batches: batch)
+    allow(Event).to receive(:request_event_by_id).and_return(event)
+
+    visit event_path(id: event, locale: :'pt-BR')
+
+    expect(page).not_to have_link 'Ver Ingressos'
+    expect(page).not_to have_link 'Ingressos Esgotados'
+    expect(page).not_to have_content "Ingressos disponíveis #{I18n.l(3.day.from_now.to_date, format: :short)}"
+    expect(page).not_to have_content "Ingressos disponíveis #{I18n.l(5.day.from_now.to_date, format: :short)}"
+    expect(page).to have_content "Ingressos disponíveis #{I18n.l(2.day.from_now.to_date, format: :short)}"
+  end
+
+  it 'com vários ingressos fora da época de venda' do
+    event = build(:event, name: 'Evento Teste 01', batches: [])
+    allow(Event).to receive(:request_event_by_id).and_return(event)
+
+    visit event_path(id: event, locale: :'pt-BR')
+
+    expect(page).not_to have_link 'Ver Ingressos'
+    expect(page).not_to have_link 'Ingressos Esgotados'
+    expect(page).to have_content 'Ainda não existem ingressos disponíveis'
   end
 end
