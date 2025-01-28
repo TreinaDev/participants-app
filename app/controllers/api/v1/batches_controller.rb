e29d373e5b96531596d434d5e87bbae3fad166be
David Martinez <davmartinezb99@gmail.com>
2025-01-28 17:15:57 -0300

    feat: Adicionado prevenção a erros na api de batches
    Co-authored-by: joaoCasteloBranco <joao.castelo.medeiros@gmail.com>

--- app/controllers/api/v1/batches_controller.rb
+++ app/controllers/api/v1/batches_controller.rb
@@ -1,11 +1,7 @@
 class Api::V1::BatchesController < Api::V1::ApiController
   def show
-    begin
-      batch_id = params[:id].to_i
-      sold_tickets = Ticket.where(batch_id: params[:id]).count
-      render status: 200, json: { id: batch_id, sold_tickets: sold_tickets }
-    rescue
-      render status: 400
-    end
+    batch_id = params[:id].to_i
+    sold_tickets = Ticket.where(batch_id: params[:id]).count
+    render status: :ok, json: { id: batch_id, sold_tickets: sold_tickets }
   end
 end
--- spec/requests/apis/tickets/tickets_api_spec.rb
+++ spec/requests/apis/tickets/tickets_api_spec.rb
@@ -35,9 +35,98 @@ describe 'Tickets API' do
       expect(response.content_type).to include 'application/json'
 
       json_response = JSON.parse(response.body)
-      puts "Params: #{json_response}"
+
       expect(json_response["sold_tickets"]).to eq 2
       expect(json_response["id"]).to eq 2
+    end
+
+    it 'e retorna zero ingerssos vendidos quando não encontra o lote' do
+      batches = [
+        {
+        id: 2,
+        name: 'PCD - Meia',
+        limit_tickets: 50,
+        start_date: 6.days.ago.to_date,
+        value: 10.00,
+        end_date: 2.month.from_now.to_date,
+        event_id: 1
+        }
+      ]
+      target_batch_id = batches[0][:id]
+      create(:ticket, batch_id: target_batch_id)
+      invalid_batch_id = 12345678
+
+      get "/api/v1/batches/#{invalid_batch_id}"
+
+      expect(response.status).to eq 200
+      expect(response.content_type).to include 'application/json'
+
+      json_response = JSON.parse(response.body)
+
+      expect(json_response["sold_tickets"]).to eq 0
+      expect(json_response["id"]).to eq 12345678
+    end
+
+    it 'e não conta tickets de outro lote' do
+      batches = [
+        {
+        id: 1,
+        name: 'Entrada - Meia',
+        limit_tickets: 20,
+        start_date: 5.days.ago.to_date,
+        value: 20.00,
+        end_date: 2.month.from_now.to_date,
+        event_id: 1
+        },
+        {
+        id: 2,
+        name: 'PCD - Meia',
+        limit_tickets: 50,
+        start_date: 6.days.ago.to_date,
+        value: 10.00,
+        end_date: 2.month.from_now.to_date,
+        event_id: 1
+        }
+      ]
+      target_batch_id = batches[1][:id]
+      other_batch_id = batches[0][:id]
+
+      build(:event, name: 'DevWeek',  event_id: 1, batches: batches)
+      create(:ticket, batch_id: target_batch_id)
+      create(:ticket, batch_id: target_batch_id)
+      create(:ticket, batch_id: other_batch_id)
+
+      get "/api/v1/batches/#{target_batch_id}"
+
+      expect(response.status).to eq 200
+      expect(response.content_type).to include 'application/json'
+
+      json_response = JSON.parse(response.body)
+
+      expect(json_response["sold_tickets"]).to eq 2
+      expect(json_response["id"]).to eq 2
+    end
+
+    it 'E falha com um erro interno' do
+      batches = [
+        {
+        id: 2,
+        name: 'PCD - Meia',
+        limit_tickets: 50,
+        start_date: 6.days.ago.to_date,
+        value: 10.00,
+        end_date: 2.month.from_now.to_date,
+        event_id: 1
+        }
+      ]
+      target_batch_id = batches[0][:id]
+      create(:ticket, batch_id: target_batch_id)
+      invalid_batch_id = 12345678
+      allow(Ticket).to receive(:where).and_raise(ActiveRecord::QueryCanceled)
+
+      get "/api/v1/batches/#{invalid_batch_id}"
+
+      expect(response.status).to eq 500
     end
   end
 end
