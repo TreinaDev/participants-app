7c919437d4991ce94682c661ebfa2386dccfa2f0
SamuelRocha91 <samuel_sr@hotmail.com.br>
2025-01-29 18:22:47 -0300

    refactor: cria dropdow de troca de idioma
    Co-authored-by: Cristiano <cristiano.mer@gmail.com>

--- spec/system/languages/user_change_language_spec.rb
+++ spec/system/languages/user_change_language_spec.rb
@@ -10,8 +10,24 @@ describe 'Usuário troca de idioma' do
     expect(page).to have_css 'h1', text: 'Available Events'
     expect(page).not_to have_css 'h1', text: 'Eventos disponíveis'
     within('nav') do
-      expect(page).to have_link 'Português'
-      expect(page).not_to have_link 'English'
+      expect(page).to have_content 'en (US)'
+      expect(page).not_to have_content 'pt-BR'
+    end
+  end
+
+  it 'e troca de inglês pra portugues' do
+    visit events_path(locale: 'pt-BR')
+    within('nav') do
+      click_on 'English'
+    end
+    within('nav') do
+      click_on 'Português'
+    end
+    expect(page).not_to have_css 'h1', text: 'Available Events'
+    expect(page).to have_css 'h1', text: 'Eventos disponíveis'
+    within('nav') do
+      expect(page).not_to have_content 'en (US)'
+      expect(page).to have_content 'pt-BR'
     end
   end
 end
