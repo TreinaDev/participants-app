e1996c6fd62e8bb3974b9de34365c2085f416c22
César Faustino <tarefas.fatec@gmail.com>
2025-01-30 15:43:56 -0300

    Aplica as migrations pendentes
    Co-authored-by: Gabriel-T-P <gabrielthiago2000@hotmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_01_29_162909) do
+ActiveRecord::Schema[8.0].define(version: 2025_01_30_142528) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
