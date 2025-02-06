184442e6b6268cce7aef3cc5db9af8aed8c8c2cb
David Martinez <davmartinezb99@gmail.com>
2025-02-06 15:42:50 -0300

    Eliminado atributo answer do item_feedbacks
    Co-authored-by: Gabriel-T-P <gabrielthiago2000@hotmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_05_205330) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_06_184046) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -82,7 +82,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_05_205330) do
   create_table "item_feedbacks", force: :cascade do |t|
     t.string "title", null: false
     t.string "comment", null: false
-    t.string "answer"
     t.integer "mark", default: 0, null: false
     t.boolean "public", null: false
     t.string "event_id", null: false
