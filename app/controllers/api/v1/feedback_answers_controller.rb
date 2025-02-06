e21350a9f890c9023920074331582c9cee7c7a5e
David Martinez <davmartinezb99@gmail.com>
2025-02-06 17:16:46 -0300

    Adiciona validações a resposta do feedback
    Co-authored-by: Gabriel-T-P <gabrielthiago2000@hotmail.com>

--- app/controllers/api/v1/feedback_answers_controller.rb
+++ app/controllers/api/v1/feedback_answers_controller.rb
@@ -3,6 +3,8 @@ class Api::V1::FeedbackAnswersController < Api::V1::ApiController
     @feedback_answers = FeedbackAnswer.new(feedback_answers_params)
     if @feedback_answers.save
       render status: 201, json: @feedback_answers.as_json
+    else
+      render status: 406, json: { errors: I18n.with_locale(:en) { @feedback_answers.errors.full_messages } }
     end
   end
 
--- spec/requests/apis/item_feedback_answers/feedback_answers_api_spec.rb
+++ spec/requests/apis/item_feedback_answers/feedback_answers_api_spec.rb
@@ -23,7 +23,7 @@ describe 'Feedback Answer API' do
       allow(Event).to receive(:request_event_by_id).and_return(event)
       login_as user
       item_feedback = create(:item_feedback, title: 'Título do feedback de item', comment: 'Comentário Padrão de item', mark: 4, event_id: event.event_id,
-              user: user, schedule_item_id: schedule_item.schedule_item_id, public: true)
+                                             user: user, schedule_item_id: schedule_item.schedule_item_id, public: true)
 
       post "/api/v1/item_feedbacks/#{item_feedback.id}/feedback_answers", params: {
             feedback_answers: {
@@ -40,6 +40,46 @@ describe 'Feedback Answer API' do
       expect(json_response['name']).to eq 'Nome do Participante Teste'
       expect(json_response['email']).to eq 'email@teste.com'
       expect(json_response['comment']).to eq 'Comentário Teste'
+      expect(json_response['item_feedback_id']).to eq item_feedback.id
+    end
+
+    it 'e retorna erro quando a criação não é válida' do
+      schedules = [
+        {
+          date: 	5.day.ago,
+          schedule_items: [
+            {
+              name:	"Palestra",
+              start_time:	5.day.ago.beginning_of_day + 9.hours,
+              end_time:	5.day.ago.beginning_of_day + 10.hours,
+              code: '1'
+            }
+          ]
+        }
+      ]
+      user = create(:user, name: 'Gabriel', last_name: 'Tavares')
+      event = build(:event, event_id: '1', start_date: 5.days.ago, end_date: 2.days.ago, name: 'DevWeek', schedules: schedules)
+      schedule_item = event.schedules[0].schedule_items[0]
+      create(:ticket, event_id: event.event_id, user: user)
+      allow(Event).to receive(:request_event_by_id).and_return(event)
+
+      login_as user
+      item_feedback = create(:item_feedback, title: 'Título do feedback de item', comment: 'Comentário Padrão de item', mark: 4, event_id: event.event_id,
+              user: user, schedule_item_id: schedule_item.schedule_item_id, public: true)
+
+      post "/api/v1/item_feedbacks/#{item_feedback.id}/feedback_answers", params: {
+            feedback_answers: {
+            name: '',
+            email: '',
+            comment: '',
+            item_feedback_id: nil
+          }
+        }
+
+      expect(response).to have_http_status 406
+      expect(response.content_type).to include 'application/json'
+      json_response = JSON.parse(response.body)
+      expect(json_response['errors']).to match_array [ "Name can't be blank", "Email can't be blank", "Comment can't be blank", "Item feedback must exist" ]
     end
   end
 end
