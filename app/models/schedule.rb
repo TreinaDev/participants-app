03ca62f9682c1c2ddf726ff695ac7c3b3152eda3
SamuelRocha91 <samuel_sr@hotmail.com.br>
2025-02-06 17:13:48 -0300

    feat: adiciona dados da api no poro de schedule, refatora build de schedule_item do factory_bot e refatora testes e show que implementa schedule
    Co-authored-by: César Faustino <cesar.faustino@fatec.sp.gov.br>

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -9,6 +9,6 @@ class Schedule
 
   def build_schedule_items(schedule_items)
     schedule_items.map { |schedule_item| ScheduleItem.new(schedule_item_id: schedule_item[:code], name: schedule_item[:name], start_time: schedule_item[:start_time],
-                                                   end_time: schedule_item[:end_time])}
+                                                   end_time: schedule_item[:end_time], description: schedule_item[:description], responsible_name: schedule_item[:responsible_name], responsible_email: schedule_item[:responsible_email], schedule_type: schedule_item[:schedule_type])}
   end
 end
