3bdeef6fc8091db9ef513d8921e598a786213264
David Martinez <davmartinezb99@gmail.com>
2025-02-06 15:28:50 -0300

    endpoint da api de feedbacks atualizado para incorporar feedbacks de items do evento
    Co-authored-by: Gabriel-T-P <gabrielthiago2000@hotmail.com

--- app/controllers/api/v1/feedbacks_controller.rb
+++ app/controllers/api/v1/feedbacks_controller.rb
@@ -2,12 +2,19 @@ class Api::V1::FeedbacksController < Api::V1::ApiController
   before_action :set_and_check_event
 
   def index
+    @item_feedbacks = ItemFeedback.where(event_id: @event.event_id)
     @feedbacks = Feedback.where(event_id: @event.event_id)
-    if @feedbacks.any?
+    if @feedbacks.any? || @item_feedbacks.any?
+      item_feedbacks = @item_feedbacks.map { |item_feedback| { id: item_feedback.id, title: item_feedback.title,
+                                                               comment: item_feedback.comment, mark: item_feedback.mark,
+                                                               user: item_feedback.user.full_name,
+                                                               schedule_item_id: item_feedback.schedule_item_id } }
+
       feedback_json = @feedbacks.map { |feedback| { id: feedback.id, title: feedback.title,
                                                     comment: feedback.comment, mark: feedback.mark,
                                                     user: feedback.user.full_name } }
-      render status: :ok, json: { event_id: @event.event_id, feedbacks: feedback_json }
+
+      render status: :ok, json: { event_id: @event.event_id, feedbacks: feedback_json, item_feedbacks: item_feedbacks }
     else
       render status: :not_found, json: { event_id: @event.event_id, error: "There is no feedbacks to this event yet" }
     end
