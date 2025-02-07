22ff2ef536f530076b17f64dc48dec44b447c850
Samuel Rocha <112505223+SamuelRocha91@users.noreply.github.com>
2025-02-07 17:32:13 -0300

    Update announcements.rb

--- spec/factories/announcements.rb
+++ spec/factories/announcements.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :announcement do
-    announcement_id { "ABGJLJ5" }
+    announcement_id { SecureRandom.alphanumeric(6) }
     title { "Dev Week" }
     description { 'Aprenda em a ganhar 10 mil dólares em um mês' }
     created_at { "2025-02-02T12:00:00.000-03:00" }
